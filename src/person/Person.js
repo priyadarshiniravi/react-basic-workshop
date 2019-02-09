72f2714288caf2c27981e5cdef641b9dd35c9bd1
Ravi Priyadarshini <pravi@equalexperts.com>
2019-02-09 23:14:43 +0530

    change name based on input

--- src/person/Person.js
+++ src/person/Person.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 const Person = (props) => {
+    let onChangeInput = (event) => props.onNameClick.bind(this, event.target.value)();
     return (
         <div>
             <div onClick={props.onNameClick.bind(this, props.name)}>{props.name}</div>
             <div>{props.age}</div>
             <div>{props.gender}</div>
             <div>{props.children}</div>
+            <input onChange={onChangeInput} value={props.name}/>
         </div>
     )
 };
